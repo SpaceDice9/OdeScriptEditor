5e52d906c467bc20b32ef888c18bc402e63c86d3
SpaceDice9 <soacecube@gmail.com>
2024-05-12 14:53:29 -0400

    Support other characters in autowrap

--- src/OdeScriptEditor/Hooks/AutoWrap.lua
+++ src/OdeScriptEditor/Hooks/AutoWrap.lua
@@ -4,6 +4,15 @@ function isCharWhitespace(char)
 	return char == "\n" or char == " "
 end
 
+local allowedWraps = {
+	{"\"", "\""},
+	{"'", "'"},
+
+	{"(", ")"},
+	{"[", "]"},
+	{"{", "}"},
+}
+
 function AutoWrap.OnScriptChange(scriptEditor, data)
 	local code = data.Code
 	local cursor = data.Cursor
@@ -18,11 +27,15 @@ function AutoWrap.OnScriptChange(scriptEditor, data)
 		local cursorChar = string.sub(code, lastCharPosition + 1, lastCharPosition + 1)
 		local nextLastChar = string.sub(code, lastCharPosition - 1, lastCharPosition - 1)
 
-		if lastChar == "\"" and lastSelectedString ~= "\"" and lastSelectedString ~= "" and (scriptEditor._LastPreviousChar ~= "\"" or nextLastChar == "\"") then
-			code = string.sub(code, 1, lastCharPosition) .. lastSelectedString .. "\"" .. string.sub(code, lastCharPosition + 1)
+		for _, wrappedCharData in allowedWraps do
+			local startingWrappedChar = wrappedCharData[1]
 
-			codeField.SelectionStart = if lastSelectedString ~= "" then lastCharPosition + 1 else -1
-			data.Cursor += lastSelectedString:len()
+			if lastChar == startingWrappedChar and lastSelectedString ~= startingWrappedChar and lastSelectedString ~= "" and (scriptEditor._LastPreviousChar ~= startingWrappedChar or nextLastChar == startingWrappedChar) then
+				code = string.sub(code, 1, lastCharPosition) .. lastSelectedString .. wrappedCharData[2] .. string.sub(code, lastCharPosition + 1)
+
+				codeField.SelectionStart = if lastSelectedString ~= "" then lastCharPosition + 1 else -1
+				data.Cursor += lastSelectedString:len()
+			end
 		end
 	end
 
